00038765a1494ea325a2aac13cebae435b97fbbf
piicollo <kapilchharahangrai@gmail.com>
2025-10-21 21:13:11 +0545

    comparision operation

---
+++ comparison.js
@@ -0,0 +1,32 @@
+// console.log(2 > 1);
+
+// let num1 = 1;
+// let num2 = "2";
+// let num3 = "02"
+// result1 = num2 > num1;
+// result2 = num3 > num1;
+// console.log(result1); 
+// console.log(result2);
+// console.log(typeof result1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log("\n");
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+console.log("\n");
+
+// ===
+
+console.log("2" === 2);
+
+// Note: Avoid this type of comparison 
--- conversionOperations.js
+++ conversionOperations.js
@@ -26,3 +26,38 @@ console.log(booleanIsLoggedIn);
 
 
 // ************************************ Operations ************************************//
+
+// let value = -3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(3%2);
+
+// let num1 = 22;
+// let num2 = "12";
+
+// let num3 = Number(num2);
+// console.log(num1 + num3);
+
+// console.log(2+3);
+// console.log(2 + "3");
+// console.log((2*3) * (4/3)); // 7.99999999
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2  ; // not readable code
+// console.log(num1, num2, num3);
+
+
+// prefix and postfix 
+
+let gameCounter = 20;
+gameCounter++;
+console.log(gameCounter);
