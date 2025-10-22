2b24d2bd6181c90e99a80fe84c60ba2cc857ea55
piicollo <kapilchharahangrai@gmail.com>
2025-10-22 13:56:47 +0545

    Summary of Data Types

---
+++ dataTypes_Summary.js
@@ -0,0 +1,61 @@
+// let num1 = 22;
+// num2 = String(num1);
+
+// console.log(num2);
+// console.log(typeof num2);
+
+// Types of data types or Data Categorized
+// 1. Primitive Data Types (& Types):
+// String, Numbers, Boolean, null, undefined, Symbol, bigInt :
+// const score = 12;
+// let scoreValue = 100.2;
+// const loggedIn = true;
+// let outSideTemp = null;
+// let userEmail; // undefined
+// // let userEmail = undefined;
+// const id = Symbol("12345");
+// const anotherId = Symbol(1233);
+// console.log(typeof anotherId);
+// console.log(typeof(id));
+
+// console.log(id);
+// console.log(anotherId);
+
+// const largeNumber = 1245n;
+// console.log(typeof largeNumber);
+
+
+
+//2. Non-Primitive Data Types (Reference )
+
+// Array, Objects, Functions 
+
+// Arrays 
+
+const userId = [1234, 1245, 12455];
+
+console.log(userId);
+console.log(typeof userId);
+
+
+// Objects 
+let studentsName = {
+    name: "Kapil Rai",
+    faculty: "BCS",
+    email: "BCS2440"
+}
+
+console.log(studentsName);
+
+// Functions In Java Script
+
+const myFunction = function() {
+    console.log("Hello World! ");
+    
+}
+
+myFunction()
+console.log("\n");
+
+
+console.log(typeof myFunction);
