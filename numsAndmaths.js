0ea5e2e8bc6b837073b42360165ac04038d5323f
piicollo <kapilchharahangrai@gmail.com>
2025-10-22 22:48:12 +0545

    nums and maths

---
+++ numsAndmaths.js
@@ -0,0 +1,42 @@
+// const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString(100));
+// console.log(typeof score);
+
+// let oneString = String(score);
+
+// console.log(score.toString());
+
+// console.log(typeof oneString);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNums = 123.25555;
+ 
+// console.log(otherNums.toPrecision(4));
+
+// const hundreds = 100000000;
+
+// console.log(hundreds.toLocaleString('en-IN')); // 10,00,00,000
+
+// ++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.max(3, 3, 5, 0, 8));
+// console.log("\n");
+
+// console.log(Math.min(4, 5, 6, 8, 2));
+
+console.log(Math.random());
+console.log(Math.random());
