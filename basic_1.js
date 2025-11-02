9783fc81124d03086d737261a768050d937554f6
piicollo <kapilchharahangrai@gmail.com>
2025-11-02 09:42:44 +0545

    typesof

--- basic_1.js
+++ basic_1.js
@@ -1,12 +1,31 @@
-console.log("Hello World! ");
+// console.log("Hello World! ");
 
-const customerId = 14533; 
-let customerName = "Kapil Rai";
-var customerAddress = "Dhankuta";
-let customerMail;
-let customerPhone = 98745123122;
+// const customerId = 14533; 
+// let customerName = "Kapil Rai";
+// var customerAddress = "Dhankuta";
+// let customerMail;
+// let customerPhone = 98745123122;
 
-console.log(customerId);
+// // console.log(customerId);
 
-console.table([customerId, customerName, customerAddress, customerMail, customerPhone]);
+// console.table([customerId, customerName, customerAddress, customerMail, customerPhone]);
+
+let userId = 12453;
+let userName = "Kapil Rai";
+let userPhoneNumber = 9842304471;
+let userMail = "kapil@mail.com";
+let userAddress = "Dhankuta-2, Dhankuta";
+
+// console.table([userId, userName, userPhoneNumber, userMail, userAddress]);
+// console.log(`User Id: ${userId}`);
+// console.log(`Username: ${userName}`);
+// console.log(`Contact: ${userPhoneNumber}`);
+// console.log(`E-mail: ${userMail}`);
+// console.log(`Address: ${userAddress}`);
+
+
+// Types of 
+console.log(typeof userId);
+console.log(typeof(userName));
+console.log(typeof(userAddress));
 
